1e1a007c635f9287d3bdf836900202a798bfd6c7
Hanson Lee <leehanson@alumni.ubc.ca>
2022-10-21 03:58:53 +0000

    User can now post new listing to database via postgresql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -170,9 +170,20 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log(property);
+  let params = [];
+  for (const key in property) {
+    params.push(property[key]);
+  }
+  return pool 
+  .query(`INSERT INTO properties (title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, params)
+  .then((result) => {
+    const output = result.rows[0];
+    return output;
+  })
+  .catch((err) => {
+    console.log("this is error", err);
+  });
 }
 exports.addProperty = addProperty;
